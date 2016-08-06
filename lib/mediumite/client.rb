9e3c120b0a9d8d2a104bb22ab111568db92b5b0a
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2016-08-06 15:45:46 +0900

    Enable to get current user

--- lib/mediumite/client.rb
+++ lib/mediumite/client.rb
@@ -1,3 +1,5 @@
+require "sawyer"
+
 module Mediumite
   class Client
     def initialize(options = {})
@@ -6,6 +8,21 @@ module Mediumite
 
     def token
       @options[:token]
+    end
+
+    def agent
+      @agent ||= Sawyer::Agent.new("https://api.medium.com/v1") do |http|
+        http.headers[:content_type] = "application/json"
+        http.authorization "Bearer", token
+      end
+    end
+
+    def request(method, path, data = {}, options = {})
+      agent.call(method, URI::Parser.new.escape(path.to_s), data, options).data
+    end
+
+    def user
+      @user ||= request :get, "me"
     end
   end
 end
